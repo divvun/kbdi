1cdd6d532d385e70359c1280f94ab59273441975
Zoey Riordan <zoey@dos.cafe>
2020-10-20 14:36:01 -0700

    fix legacy builds

--- src/keyboard_legacy.rs
+++ src/keyboard_legacy.rs
@@ -1,7 +1,4 @@
-use winreg::RegKey;
-use winreg::enums::{
-    HKEY_CURRENT_USER, HKEY_USERS, HKEY_LOCAL_MACHINE, KEY_READ, KEY_WRITE
-};
+use registry::{Hive, Data, RegKey, Security};
 use crate::keyboard::{Error, KeyboardRegKey};
 use crate::types::*;
 use crate::platform::*;
@@ -26,39 +23,37 @@ pub fn enable(tag: &str, product_code: &str) -> Result<(), Error> {
 fn base_regkey(is_all_users: bool) -> RegKey {
     match is_all_users {
         true => {
-            RegKey::predef(HKEY_USERS)
-                .open_subkey_with_flags(r".DEFAULT", KEY_READ | KEY_WRITE)
-                .unwrap()
+            Hive::Users.open(".DEFAULT", Security::Read | Security::Write).unwrap()
         },
-        false => RegKey::predef(HKEY_CURRENT_USER)
+        false => Hive::CurrentUser.open("", Security::Read | Security::Write).unwrap()
     }
 }
 
 fn kbd_layout_sub_regkey(is_all_users: bool) -> RegKey {
     base_regkey(is_all_users)
-        .open_subkey_with_flags(r"Keyboard Layout\Substitutes", KEY_READ | KEY_WRITE)
+        .open(r"Keyboard Layout\Substitutes", Security::Read | Security::Write)
         .unwrap()
 }
 
 fn kbd_layout_preload_regkey(is_all_users: bool) -> RegKey {
     base_regkey(is_all_users)
-        .open_subkey_with_flags(r"Keyboard Layout\Preload", KEY_READ | KEY_WRITE)
+        .open(r"Keyboard Layout\Preload", Security::Read | Security::Write)
         .unwrap()
 }
 
 /// Substitute IDs begin with 0000, then increment to d001, and continue incrementing dXXX.
 fn next_substitute_id(suffix: u16) -> u32 {
-    let prefix: u16 = RegKey::predef(HKEY_CURRENT_USER)
-        .open_subkey_with_flags(r"Keyboard Layout\Substitutes", KEY_READ)
+    let prefix: u16 = Hive::CurrentUser
+        .open(r"Keyboard Layout\Substitutes", Security::Read)
         .unwrap()
-        .enum_values()
+        .values()
         .fold(0u16, |acc, x| {
-            let (k, _) = x.unwrap();
+            let name = x.unwrap().name().to_string_lossy();
 
-            if let Ok(kk) = u32::from_str_radix(&k, 16) {
-                if (kk as u16) == suffix {
+            if let Ok(val) = u32::from_str_radix(&name, 16) {
+                if (val as u16) == suffix {
                     // Move high bits down
-                    let v = (kk >> 16) as u16;
+                    let v = (val >> 16) as u16;
 
                     if v >= acc {
                         return if v == 0 {
@@ -81,13 +76,13 @@ fn next_substitute_id(suffix: u16) -> u32 {
 #[cfg(feature = "legacy")]
 fn next_preload_id(is_all_users: bool) -> u32 {
     base_regkey(is_all_users)
-        .open_subkey_with_flags(r"Keyboard Layout\Preload", KEY_READ)
+        .open(r"Keyboard Layout\Preload", Security::Read)
         .unwrap()
-        .enum_values()
+        .values()
         .fold(1u32, |acc, x| {
-            let (k, _) = x.unwrap();
+            let name = x.unwrap().name().to_string_lossy();
 
-            if let Ok(v) = u32::from_str_radix(&k, 10) {
+            if let Ok(v) = u32::from_str_radix(&name, 10) {
                 if v >= acc {
                     v + 1
                 } else {
